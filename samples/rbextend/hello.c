d1864fe17c0739fb71e4211d18f8bf41f6ac33ee
Bertram Scharpf <software@bertram-scharpf.de>
2013-04-17 15:40:03 +0200

    Sample rbhello

---
+++ samples/rbextend/hello.c
@@ -0,0 +1,43 @@
+/*
+ *  hello.c  --  Say "Hello, world!"
+ */
+
+
+#include "hello.h"
+
+#if   HAVE_HEADER_RUBY_H
+    #include <ruby.h>
+#elif HAVE_HEADER_RUBY_RUBY_H
+    #include <ruby/ruby.h>
+#else
+    #error "Ruby doesn't seem to be installed."
+#endif
+
+
+static ID id_puts;
+
+static VALUE rb_obj_hello_bang( VALUE obj);
+
+
+/*
+ *  call-seq:
+ *     obj.hello!   => nil
+ *
+ *  Prints "Hello, world!".
+ *
+ */
+
+VALUE rb_obj_hello_bang( VALUE obj)
+{
+    return rb_funcall( obj, id_puts, 1, rb_str_new2( "Hello, world!"));
+}
+
+
+
+void Init_hello( void)
+{
+    rb_define_method( rb_cObject, "hello!", &rb_obj_hello_bang, 0);
+
+    id_puts = rb_intern( "puts");
+}
+
---
+++ samples/rbextend/hello.h
@@ -0,0 +1,11 @@
+/*
+ *  hello.h  --  Say "Hello, world!"
+ */
+
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+extern void Init_hello( void);
+
+#endif
+
