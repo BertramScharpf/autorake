48866a68ceec6b5e097405dce4f57cb25538ed98
Bertram Scharpf <software@bertram-scharpf.de>
2014-12-05 23:19:05 +0100

    Version bump

--- lib/autorake/version.rb
+++ lib/autorake/version.rb
@@ -5,7 +5,7 @@
 module Autorake
 
   NAME        = "autorake"
-  VERSION     = "2.4.1".freeze
+  VERSION     = "2.4.2".freeze
   SUMMARY     = "Automake like project config before Rake build or install."
 
   DESCRIPTION = <<EOT
