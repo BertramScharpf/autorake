8c4b9a26fb1ea1fd325aed084f7a4ea6c6d4be63
Bertram Scharpf <software@bertram-scharpf.de>
2015-08-21 00:00:29 +0200

    Well-documented example

--- examples/plainc/hello.c
+++ examples/plainc/hello.c
@@ -7,14 +7,30 @@
     #define SAY_IT
 #endif
 
+#ifdef FEATURE_NOW
+    #include <string.h>
+#endif
+
+
+#define CONFIGFILE WITH_SYSCONF "/myproj.cfg"
+#define DATAFILE   WITH_MYPROJ "/somedata"
+
+
 int main( int argc, char **argv)
 {
+
 #ifdef SAY_IT
     printf( "Hello, world!\n");
 #endif
+
 #ifdef FEATURE_NOW
-    printf( "Now is the time.\n");
+    if (strcmp( WITH_FOO, "dummy") == 0)
+        printf( "Now is the time.\n");
 #endif
+
+    printf( "Config file is: '%s'\n", CONFIGFILE);
+    printf( "Data   file is: '%s'\n", DATAFILE);
+
     return 0;
 }
 
